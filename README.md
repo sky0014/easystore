292b02d3cbb5a18d2071a9cb19a4a6df22f6f996
sky0014 <sky-wang@qq.com>
2021-08-31 18:14:53 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,149 @@
+# easystore
+
+An easy store for react and hooks.
+
+## Install
+
+```bash
+npm install @sky0014/easystore
+```
+
+## Usage
+
+```js
+// store.js
+import { createStore, register } from "@sky0014/easystore";
+import header from "./header/module";
+import todo from "../page/todo/module";
+
+// 支持动态注册
+register(header);
+register(todo);
+
+export default createStore({
+  debug: true,
+});
+
+
+// module.js
+export default {
+  // 模块唯一id
+  id: "todo",
+
+  // 默认state
+  state: [
+    {
+      id: 0,
+      todo: "something need todo",
+      status: 0,
+    },
+  ],
+
+  // 同步方法
+  reducers: {
+    addTodo(state, todo) {
+      const id = state.length;
+      state.push({
+        id,
+        todo,
+        status: 0,
+      });
+    },
+
+    completeTodo(state, id) {
+      const ele = state.find((v) => v.id === id);
+      if (ele) {
+        ele.status = 1;
+      }
+    },
+  },
+
+  // 异步方法
+  effects: {
+    async load(produce) {
+      const result = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(["1111111", "22222222", "333333", "55555555", "44444"]);
+        }, 2000);
+      });
+      // 1. 调用同步方法修改
+      // result.forEach(this.addTodo)
+      // or 2. 使用produce直接修改state
+      produce((state) => {
+        result.forEach((todo) => {
+          const id = state.length;
+          state.push({
+            id,
+            todo,
+            status: 0,
+          });
+        });
+      });
+    },
+  },
+};
+
+
+// todo.js
+import React, { useEffect } from "react";
+import { call, useData } from "@sky0014/easystore";
+
+function Todo() {
+  console.log("render todo");
+
+  // 支持多层级获取，如：todo.a.b.c
+  // 在非组件中，可使用 getData 方法获取
+  const todo = useData("todo") || [];
+
+  useEffect(() => {
+    call("todo/load");
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="text"
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            call("todo/addTodo", e.target.value);
+            e.target.value = "";
+          }
+        }}
+      ></input>
+      {todo
+        .concat()
+        .sort((a, b) => a.status - b.status)
+        .map((v) => (
+          <div key={v.id}>
+            <input
+              type="checkbox"
+              onChange={() => {
+                call("todo/completeTodo", v.id);
+              }}
+              checked={v.status}
+              disabled={v.status}
+            />{" "}
+            {v.todo}
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default Todo;
+
+```
+
+## Publish
+
+If your first time publish a package, login first:
+
+```bash
+npm login --registry=http://registry.npmjs.org
+```
+
+Then you can publish:
+
+```bash
+npm run pub
+```
---
+++ src/index.js
@@ -0,0 +1,163 @@
+import produce from "immer";
+import { useSelector } from "react-redux";
+import { applyMiddleware, compose, createStore as _createStore } from "redux";
+import reduxLogger from "redux-logger";
+import logger from "@sky0014/logger";
+
+logger.initLogger({
+  enable: false,
+  prefix: "easystore",
+});
+
+let store;
+
+const TYPE_MODULE_REGISTER = "@@easystore/MODULE_REGISTER";
+
+const moduleMap = {};
+
+const initialState = {};
+
+const actions = {};
+
+const reducers = {
+  [TYPE_MODULE_REGISTER](state, action) {
+    const module = action.payload;
+    state[module.id] = module.state;
+  },
+};
+
+function getModuleId(type) {
+  const index = type.indexOf("/");
+  if (index === -1) {
+    return null;
+  }
+  return type.substring(0, index);
+}
+
+function internalGetData(state, path, defaultValue) {
+  const array = path.split(".");
+  let value = state;
+
+  for (let i = 0, len = array.length; i < len; i++) {
+    if (!value || !Object.prototype.hasOwnProperty.call(value, array[i])) {
+      return defaultValue;
+    }
+    value = value[array[i]];
+  }
+
+  return value;
+}
+
+const reducer = produce((state, action) => {
+  const r = reducers[action.type];
+  if (!r && !action.skipCheck) {
+    if (action.type.indexOf("@@redux") === -1) {
+      logger.log(`no reducer for action: `, action);
+    }
+    return;
+  }
+
+  if (action.type === TYPE_MODULE_REGISTER) {
+    r(state, action);
+    return;
+  }
+
+  const id = getModuleId(action.type);
+  if (!id) {
+    logger.log(`action must have a module id: `, action);
+    return;
+  }
+
+  if (action.produce) {
+    action.produce(state[id]);
+  } else if (r) {
+    r(state[id], ...action.payload);
+  }
+});
+
+export function getData(path) {
+  if (!store) {
+    throw new Error("you must create store first!");
+  }
+
+  return internalGetData(store.getState(), path);
+}
+
+// react hooks
+// 不给默认值参数，以免上层使用方式不对造成不必要的组件刷新
+export function useData(path) {
+  return useSelector((state) => internalGetData(state, path));
+}
+
+export function register(m) {
+  if (moduleMap[m.id]) {
+    throw new Error(`module id=${m.id} already exist!`);
+  }
+
+  moduleMap[m.id] = m;
+
+  const localActions = (m[Symbol("actions")] = {});
+
+  m.reducers &&
+    Object.keys(m.reducers).forEach((k) => {
+      const path = `${m.id}/${k}`;
+      reducers[path] = m.reducers[k];
+      // create actions
+      actions[path] = localActions[k] = (...params) => {
+        store.dispatch({
+          type: path,
+          payload: params,
+        });
+      };
+    });
+
+  m.effects &&
+    Object.keys(m.effects).forEach((k) => {
+      const produce = (func) => {
+        store.dispatch({
+          type: `${m.id}/${k} async PRODUCE`,
+          skipCheck: true,
+          produce: func,
+        });
+      };
+      actions[`${m.id}/${k}`] = localActions[k] = async (...params) => {
+        store.dispatch({
+          type: `${m.id}/${k} async START`,
+          skipCheck: true,
+        });
+        return await m.effects[k].bind(localActions)(produce, ...params);
+      };
+    });
+
+  if (!store) {
+    initialState[m.id] = m.state;
+  } else {
+    store.dispatch({
+      type: TYPE_MODULE_REGISTER,
+      payload: m,
+    });
+  }
+}
+
+export async function call(path, ...params) {
+  const action = actions[path];
+
+  if (!action) {
+    logger.log(`wrong action called: ${path}`);
+    return;
+  }
+
+  return await action(...params);
+}
+
+export function createStore({ middlewares, debug } = {}) {
+  if (!middlewares) {
+    middlewares = [];
+  }
+  if (debug) {
+    logger.setEnable(true);
+    middlewares.unshift(reduxLogger);
+  }
+  middlewares = middlewares.map((v) => applyMiddleware(v));
+  return (store = _createStore(reducer, initialState, compose(...middlewares)));
+}
